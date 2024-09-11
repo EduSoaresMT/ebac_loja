b61f4fd4096abc650510a01986b909fb732dd333
Eduardo Soares <148549393+EduSoaresMT@users.noreply.github.com>
2024-09-11 20:04:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Função simples para adicionar ao carrinho
+document.querySelectorAll('.add-to-cart').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Produto adicionado ao carrinho!');
+    });
+});
